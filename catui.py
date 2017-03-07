a1df1492837ca2a1566895d9176b94a7535401f2
Steve Barskey <steve@Mac.local>
2017-03-06 20:58:01 -0800

    removed .pyc files. Uncommented RPIO

--- catui.py
+++ catui.py
@@ -2,9 +2,9 @@ import pygame
 import os
 import pygameui as ui
 import logging
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import gameboards
- 
+
 PINS = {
 	'11': 11,
 	'12': 12,
@@ -15,11 +15,9 @@ PINS = {
 }
 
 #Setup the GPIOs as outputs - only 4 and 17 are available
-#GPIO.setmode(GPIO.BCM)
-#for gpio_port in PINS.items:
-#	GPIO.setup(gpio_port, GPIO.OUT)
-#GPIO.setup(4, GPIO.OUT)
-#GPIO.setup(17, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+for gpio_port in PINS.items:
+	GPIO.setup(gpio_port, GPIO.OUT)
  
 log_format = '%(asctime)-6s: %(name)s - %(levelname)s - %(message)s'
 console_handler = logging.StreamHandler()
@@ -27,7 +25,7 @@ console_handler.setFormatter(logging.Formatter(log_format))
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 logger.addHandler(console_handler)
- 
+
 os.putenv('SDL_FBDEV', '/dev/fb1')
 os.putenv('SDL_MOUSEDRV', 'TSLIB')
 os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
@@ -39,10 +37,10 @@ MAXCOLS = 3
 GameBoard = gameboards.GameBoard()
 
 class MainMenu(ui.Scene):
-	
+
 	def __init__(self):
 		ui.Scene.__init__(self)
-		
+
 		self.image_view = ui.view_for_image_named('bg', True)
 		self.image_view.frame.right = LCD_WIDTH
 		self.image_view.frame.top = 0
@@ -51,7 +49,7 @@ class MainMenu(ui.Scene):
 		self.title_rect = ui.Rect(0, 0, LCD_WIDTH, 60)
 		self.title = ui.Label(self.title_rect, 'Cinematronics Audio Tester')
 		self.add_child(self.title)
-		
+
 		btn_w = 130
 		btn_h = 50
 		space = 20
@@ -68,7 +66,7 @@ class MainMenu(ui.Scene):
 			else:
 				col = 0
 				row += 1
-		
+
 	def game_btn_clicked(self, btn, mbtn):
 		sm = SoundMenu()
 		sm.selected_game = btn.text
@@ -80,9 +78,9 @@ class SoundMenu(ui.Scene):
 
 	def __init__(self):
 		ui.Scene.__init__(self)
-		
+
 		self.selected_game = ''
-		
+
 		self.image_view = ui.view_for_image_named('bg', True)
 		self.image_view.frame.right = LCD_WIDTH
 		self.image_view.frame.top = 0
@@ -92,7 +90,7 @@ class SoundMenu(ui.Scene):
 		self.title_rect = ui.Rect(0, 0, LCD_WIDTH, 40)
 		self.title = ui.Label(self.title_rect, 'Activate Sound for ' + self.selected_game + ':')
 		self.add_child(self.title)
-		
+
 		btn_w = 130
 		btn_h = 40
 		space = 15
@@ -112,22 +110,22 @@ class SoundMenu(ui.Scene):
 			else:
 				col = 0
 				row += 1
-		
+
 		back_btn = ui.Button(ui.Rect(0, LCD_HEIGHT - btn_h, btn_w, btn_h), '<< Back')
 		back_btn.on_clicked.connect(self.back_btn_clicked)
 		self.add_child(back_btn)
-		
+
 		adv_btn = ui.Button(ui.Rect(LCD_WIDTH - btn_w, LCD_HEIGHT - btn_h, btn_w, btn_h), 'Avanced >')
 		adv_btn.on_clicked.connect(self.adv_btn_clicked)
 		self.add_child(adv_btn)
-	
+
 	def init_board(self, btn, mbtn):
 		logger.info('Initialize clicked')
-	
+
 	def play_sound(self, btn, mbtn):
 		#GPIO.output(17, False)
 		logger.info(btn.text)
-		
+
 	def back_btn_clicked(self, btn, mbtn):
 		ui.scene.pop()
 		ui.scene.push(MainMenu())
@@ -143,9 +141,9 @@ class AdvMenu(ui.Scene):
 
 	def __init__(self):
 		ui.Scene.__init__(self)
-		
+
 		self.selected_game = ''
-		
+
 		self.image_view = ui.view_for_image_named('bg', True)
 		self.image_view.frame.right = LCD_WIDTH
 		self.image_view.frame.top = 0
@@ -154,12 +152,12 @@ class AdvMenu(ui.Scene):
 		self.title_rect = ui.Rect(0, 0, LCD_WIDTH, 40)
 		self.title = ui.Label(self.title_rect, 'Toggle/Pulse pins directly:')
 		self.add_child(self.title)
-		
+
 	def set_contents(self):
 
 		btn_w = 60
 		btn_h = 40
-		
+
 		# toggle row
 		lbl = ui.Label(ui.Rect(10, 100, 70, btn_h), 'Toggle')
 		self.add_child(lbl)
@@ -167,7 +165,7 @@ class AdvMenu(ui.Scene):
 			pin = 11 + col
 			x = 90 + col * (btn_w + 5)
 			y = 100
-			
+
 			tgl_btn = ui.Button(ui.Rect(x, y, btn_w, btn_h), str(pin))
 			tgl_btn.on_clicked.connect(self.tgl_btn_clicked)
 			self.add_child(tgl_btn)
@@ -179,7 +177,7 @@ class AdvMenu(ui.Scene):
 			pin = 11 + col
 			x = 90 + col * (btn_w + 5)
 			y = 100 + (btn_h + 30)
-			
+
 			pls_btn = ui.Button(ui.Rect(x, y, btn_w, btn_h), str(pin))
 			pls_btn.on_clicked.connect(self.pls_btn_clicked)
 			self.add_child(pls_btn)
@@ -187,18 +185,18 @@ class AdvMenu(ui.Scene):
 		back_btn = ui.Button(ui.Rect(0, LCD_HEIGHT - btn_h, btn_w * 2, btn_h), '<< Back')
 		back_btn.on_clicked.connect(self.back_btn_clicked)
 		self.add_child(back_btn)
-		
+
 		adv_btn = ui.Button(ui.Rect(LCD_WIDTH - btn_w * 2, LCD_HEIGHT - btn_h, btn_w * 2, btn_h), '< Easy')
 		adv_btn.on_clicked.connect(self.adv_btn_clicked)
 		self.add_child(adv_btn)
-	
+
 	def init_board(self, btn, mbtn):
 		logger.info('Initialize clicked')
-	
+
 	def tgl_btn_clicked(self, btn, mbtn):
 		#GPIO.output(17, False)
 		logger.info('Toggle:' + btn.text)
-		
+
 	def pls_btn_clicked(self, btn, mbtn):
 		#GPIO.output(17, False)
 		logger.info('Pulse:' + btn.text)
@@ -213,12 +211,12 @@ class AdvMenu(ui.Scene):
 		sm.set_contents()
 		ui.scene.pop()
 		ui.scene.push(sm)
-		
+
 if __name__ == '__main__':
-	
+
 	ui.init('Cinematronics Audio Tester', (LCD_WIDTH, LCD_HEIGHT))
 	pygame.mouse.set_visible(False)
-	
+
 	ui.scene.push(MainMenu())
-	
+
 	ui.run()
