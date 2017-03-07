ce2ba29f991b1e8f2f01e444a33ccb939f8c6ca9
barskey <barskey@gmail.com>
2017-03-06 16:51:31 -0800

    Menu system working. Need to add functions for writing to gpio. Not tested on pi.

--- catui.py
+++ catui.py
@@ -2,12 +2,24 @@ import pygame
 import os
 import pygameui as ui
 import logging
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+import gameboards
  
+PINS = {
+	'11': 11,
+	'12': 12,
+	'13': 13,
+	'14': 14,
+	'15': 15,
+	'16': 16
+}
+
 #Setup the GPIOs as outputs - only 4 and 17 are available
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
-GPIO.setup(17, GPIO.OUT)
+#GPIO.setmode(GPIO.BCM)
+#for gpio_port in PINS.items:
+#	GPIO.setup(gpio_port, GPIO.OUT)
+#GPIO.setup(4, GPIO.OUT)
+#GPIO.setup(17, GPIO.OUT)
  
 log_format = '%(asctime)-6s: %(name)s - %(levelname)s - %(message)s'
 console_handler = logging.StreamHandler()
@@ -20,42 +32,193 @@ os.putenv('SDL_FBDEV', '/dev/fb1')
 os.putenv('SDL_MOUSEDRV', 'TSLIB')
 os.putenv('SDL_MOUSEDEV', '/dev/input/touchscreen')
 
-MARGIN = 20
- 
-class PiTft(ui.Scene):
-    def __init__(self):
-        ui.Scene.__init__(self)
- 
-        self.on17_button = ui.Button(ui.Rect(MARGIN, MARGIN, 130, 90), '17 on')
-        self.on17_button.on_clicked.connect(self.gpi_button)
-        self.add_child(self.on17_button)
- 
-        self.on4_button = ui.Button(ui.Rect(170, MARGIN, 130, 90), '4 on')
-        self.on4_button.on_clicked.connect(self.gpi_button)
-        self.add_child(self.on4_button)
- 
-        self.off17_button = ui.Button(ui.Rect(MARGIN, 130, 130, 90), '17 off')
-        self.off17_button.on_clicked.connect(self.gpi_button)
-        self.add_child(self.off17_button)
- 
-        self.off4_button = ui.Button(ui.Rect(170, 130, 130, 90), '4 off')
-        self.off4_button.on_clicked.connect(self.gpi_button)
-        self.add_child(self.off4_button)
- 
-    def gpi_button(self, btn, mbtn):
-        logger.info(btn.text)
-         
-        if btn.text == '17 on':
-            GPIO.output(17, False)
-        elif btn.text == '4 on':
-            GPIO.output(4, False)
-        elif btn.text == '17 off':
-            GPIO.output(17, True)
-        elif btn.text == '4 off':
-            GPIO.output(4, True)
- 
-ui.init('Raspberry Pi UI', (480, 320))
-pygame.mouse.set_visible(False)
-ui.scene.push(PiTft())
+LCD_WIDTH = 480
+LCD_HEIGHT = 320
+MAXCOLS = 3
 
-ui.run()
+GameBoard = gameboards.GameBoard()
+
+class MainMenu(ui.Scene):
+	
+	def __init__(self):
+		ui.Scene.__init__(self)
+		
+		self.image_view = ui.view_for_image_named('bg', True)
+		self.image_view.frame.right = LCD_WIDTH
+		self.image_view.frame.top = 0
+		self.add_child(self.image_view)
+
+		self.title_rect = ui.Rect(0, 0, LCD_WIDTH, 60)
+		self.title = ui.Label(self.title_rect, 'Cinematronics Audio Tester')
+		self.add_child(self.title)
+		
+		btn_w = 130
+		btn_h = 50
+		space = 20
+		row = 0
+		col = 0
+		for game in GameBoard.Boards.iterkeys():
+			x = space + col * (btn_w + space)
+			y = 75 + row * (btn_h + space)
+			btn = ui.Button(ui.Rect(x, y, btn_w, btn_h ), game)
+			btn.on_clicked.connect(self.game_btn_clicked)
+			self.add_child(btn)
+			if col < MAXCOLS - 1:
+				col += 1
+			else:
+				col = 0
+				row += 1
+		
+	def game_btn_clicked(self, btn, mbtn):
+		sm = SoundMenu()
+		sm.selected_game = btn.text
+		sm.set_contents()
+		ui.scene.pop()
+		ui.scene.push(sm)
+
+class SoundMenu(ui.Scene):
+
+	def __init__(self):
+		ui.Scene.__init__(self)
+		
+		self.selected_game = ''
+		
+		self.image_view = ui.view_for_image_named('bg', True)
+		self.image_view.frame.right = LCD_WIDTH
+		self.image_view.frame.top = 0
+		self.add_child(self.image_view)
+
+	def set_contents(self):
+		self.title_rect = ui.Rect(0, 0, LCD_WIDTH, 40)
+		self.title = ui.Label(self.title_rect, 'Activate Sound for ' + self.selected_game + ':')
+		self.add_child(self.title)
+		
+		btn_w = 130
+		btn_h = 40
+		space = 15
+		btn = ui.Button(ui.Rect(space + 15, 55, btn_w, btn_h), 'Init')
+		btn.on_clicked.connect(self.init_board)
+		self.add_child(btn)
+		row = 0
+		col = 1
+		for sound in GameBoard.Boards[self.selected_game]['sounds']:
+			x = space + 15 + col * (btn_w + space)
+			y = 55 + row * (btn_h + space)
+			btn = ui.Button(ui.Rect(x, y, btn_w, btn_h), sound[0])
+			btn.on_clicked.connect(self.play_sound)
+			self.add_child(btn)
+			if col < MAXCOLS - 1:
+				col += 1
+			else:
+				col = 0
+				row += 1
+		
+		back_btn = ui.Button(ui.Rect(0, LCD_HEIGHT - btn_h, btn_w, btn_h), '<< Back')
+		back_btn.on_clicked.connect(self.back_btn_clicked)
+		self.add_child(back_btn)
+		
+		adv_btn = ui.Button(ui.Rect(LCD_WIDTH - btn_w, LCD_HEIGHT - btn_h, btn_w, btn_h), 'Avanced >')
+		adv_btn.on_clicked.connect(self.adv_btn_clicked)
+		self.add_child(adv_btn)
+	
+	def init_board(self, btn, mbtn):
+		logger.info('Initialize clicked')
+	
+	def play_sound(self, btn, mbtn):
+		#GPIO.output(17, False)
+		logger.info(btn.text)
+		
+	def back_btn_clicked(self, btn, mbtn):
+		ui.scene.pop()
+		ui.scene.push(MainMenu())
+
+	def adv_btn_clicked(self, btn, mbtn):
+		am = AdvMenu()
+		am.selected_game = self.selected_game
+		am.set_contents()
+		ui.scene.pop()
+		ui.scene.push(am)
+
+class AdvMenu(ui.Scene):
+
+	def __init__(self):
+		ui.Scene.__init__(self)
+		
+		self.selected_game = ''
+		
+		self.image_view = ui.view_for_image_named('bg', True)
+		self.image_view.frame.right = LCD_WIDTH
+		self.image_view.frame.top = 0
+		self.add_child(self.image_view)
+
+		self.title_rect = ui.Rect(0, 0, LCD_WIDTH, 40)
+		self.title = ui.Label(self.title_rect, 'Toggle/Pulse pins directly:')
+		self.add_child(self.title)
+		
+	def set_contents(self):
+
+		btn_w = 60
+		btn_h = 40
+		
+		# toggle row
+		lbl = ui.Label(ui.Rect(10, 100, 70, btn_h), 'Toggle')
+		self.add_child(lbl)
+		for col in range(6):
+			pin = 11 + col
+			x = 90 + col * (btn_w + 5)
+			y = 100
+			
+			tgl_btn = ui.Button(ui.Rect(x, y, btn_w, btn_h), str(pin))
+			tgl_btn.on_clicked.connect(self.tgl_btn_clicked)
+			self.add_child(tgl_btn)
+
+		# pulse row
+		lbl = ui.Label(ui.Rect(10, (100 + btn_h + 30), 70, btn_h), 'Pulse')
+		self.add_child(lbl)
+		for col in range(6):
+			pin = 11 + col
+			x = 90 + col * (btn_w + 5)
+			y = 100 + (btn_h + 30)
+			
+			pls_btn = ui.Button(ui.Rect(x, y, btn_w, btn_h), str(pin))
+			pls_btn.on_clicked.connect(self.pls_btn_clicked)
+			self.add_child(pls_btn)
+
+		back_btn = ui.Button(ui.Rect(0, LCD_HEIGHT - btn_h, btn_w * 2, btn_h), '<< Back')
+		back_btn.on_clicked.connect(self.back_btn_clicked)
+		self.add_child(back_btn)
+		
+		adv_btn = ui.Button(ui.Rect(LCD_WIDTH - btn_w * 2, LCD_HEIGHT - btn_h, btn_w * 2, btn_h), '< Easy')
+		adv_btn.on_clicked.connect(self.adv_btn_clicked)
+		self.add_child(adv_btn)
+	
+	def init_board(self, btn, mbtn):
+		logger.info('Initialize clicked')
+	
+	def tgl_btn_clicked(self, btn, mbtn):
+		#GPIO.output(17, False)
+		logger.info('Toggle:' + btn.text)
+		
+	def pls_btn_clicked(self, btn, mbtn):
+		#GPIO.output(17, False)
+		logger.info('Pulse:' + btn.text)
+
+	def back_btn_clicked(self, btn, mbtn):
+		ui.scene.pop()
+		ui.scene.push(MainMenu())
+
+	def adv_btn_clicked(self, btn, mbtn):
+		sm = SoundMenu()
+		sm.selected_game = self.selected_game
+		sm.set_contents()
+		ui.scene.pop()
+		ui.scene.push(sm)
+		
+if __name__ == '__main__':
+	
+	ui.init('Cinematronics Audio Tester', (LCD_WIDTH, LCD_HEIGHT))
+	pygame.mouse.set_visible(False)
+	
+	ui.scene.push(MainMenu())
+	
+	ui.run()
---
+++ gameboards.py
@@ -0,0 +1,134 @@
+#// Armor Attack
+AAInit = [
+	{'pin11': [False, True, True, True, True, True, True, True, False, False, False, False, False, False, False, False, False]},
+	{'pin12': [False, False, True, False, True, False, True, False, False, True, False, True, False, True, False, False, False]},
+	{'pin13': [False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, False, False]},
+	{'pin14': [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]},
+	{'pin15': [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]},
+	{'pin16': [True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True]}
+]
+
+AASounds = [
+	[ 'Tank En.',    'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 1],
+	[ 'Beep En.',    'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 2],
+	[ 'Chopper En.', 'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 3],
+	[ 'Tank Fire',      'ACT_PLS', 1, 'ST_AAS', 0x0001, 0x0001, 7],
+	[ 'Hi Explosion',   'ACT_PLS', 1, 'ST_AAS', 0x0001, 0x0001, 6],
+	[ 'Jeep Fire',      'ACT_PLS', 1, 'ST_AAS', 0x0001, 0x0001, 5],
+	[ 'Lo Explosion',   'ACT_PLS', 1, 'ST_AAS', 0x0001, 0x0001, 4],
+	[ 'Tank Speed',     'ACT_SET', 0, 'ST_AAS', 0x000F, 0x0000, 0]
+]
+
+ROSounds = [
+	[ 'Explsn',         'ACT_PLS', 0, 'ST_OUT', 0x0001, 0x0001, 7],
+	[ 'Laser',             'ACT_PLS', 4, 'ST_OUT', 0x0001, 0x0001, 4],
+	[ 'Torpedo',           'ACT_PLS', 3, 'ST_OUT', 0x0001, 0x0001, 3],
+	[ 'BG Speed',  'ACT_SET', 0, 'ST_ROS', 0x0007, 0x0000, 3],
+	[ 'BG En.', 'ACT_TGL', 0, 'ST_ROS', 0x0001, 0x0001, 2],
+	[ 'Beep',              'ACT_TGL', 0, 'ST_ROS', 0x0001, 0x0001, 1],
+	[ 'Motor',             'ACT_TGL', 0, 'ST_ROS', 0x0001, 0x0001, 0]
+]
+
+#// Solar Quest
+SQSounds = [
+	[ 'Loud Explsn', 'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  7],
+	[ 'Soft Explsn', 'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  6],
+	[ 'Thrust',         'ACT_TGL', 0, 'ST_SQ1', 0x0001, 0x0001,  5],
+	[ 'Fire',           'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  4],
+	[ 'Capture',        'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  3],
+	[ 'Nuke',           'ACT_TGL', 0, 'ST_SQ1', 0x0001, 0x0000,  2],
+	[ 'Photon',         'ACT_PLS', 1, 'ST_SQ1', 0x0001, 0x0001,  1],
+	[ 'Music En.',   'ACT_TGL', 0, 'ST_SQ2', 0x0001, 0x0000, 15],
+	[ 'Music Vol',   'ACT_SET', 0, 'ST_SQ2', 0x0007, 0x0007, 12],
+	[ 'Music Pitch',    'ACT_SET', 0, 'ST_SQ2', 0x0FFF, 0x0000,  0]
+]
+
+#// Space War
+SWSounds = [
+	[ 'Explosion',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0000, 0],
+	[ 'Fire',         'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0000, 1],
+	[ 'Thrust 1',     'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 2],
+	[ 'Thrust 2',     'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 3],
+	[ 'Sound En.', 'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 4]
+]
+
+#// Star Castle
+SCSounds = [
+	[ 'Loud Explsn',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 1],
+	[ 'Soft Explsn',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 2],
+	[ 'Laser En.',      'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 3],
+	[ 'Fire Ball En.',  'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 7],
+	[ 'Shield En.',     'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 6],
+	[ 'Star En.',       'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0000, 5],
+	[ 'Thrust En.',     'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 4],
+	[ 'BG En.', 'ACT_TGL', 0, 'ST_AAS', 0x0001, 0x0001, 3],
+	[ 'BG Speed',  'ACT_SET', 0, 'ST_AAS', 0x0007, 0x0000, 0]
+]
+
+#// Star Hawk
+SHSounds = [
+	[ 'Explosion',     'ACT_PLS', 0, 'ST_OUT', 0x0001, 0x0000, 0],
+	[ 'Laser 1',       'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0000, 1],
+	[ 'Laser 2',       'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0000, 2],
+	[ 'K On/Off',      'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 3],
+	[ 'Master On/Off', 'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0001, 4],
+	[ 'K Exit',        'ACT_TGL', 0, 'ST_OUT', 0x0001, 0x0000, 7]
+]
+
+#// Sundance
+SDSounds = [
+	[ 'Bong',      'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 0],
+	[ 'Whoosh',    'ACT_PLS', 0, 'ST_OUT', 0x0001, 0x0001, 1],
+	[ 'Explsn', 'ACT_PLS', 0, 'ST_OUT', 0x0001, 0x0001, 2],
+	[ 'Ping 1',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 3],
+	[ 'Ping 2',    'ACT_PLS', 1, 'ST_OUT', 0x0001, 0x0001, 4],
+	[ 'Hatch',     'ACT_PLS', 6, 'ST_OUT', 0x0001, 0x0001, 7]
+];
+
+#// Tailgunner
+TGSounds = [
+	[ 'Explsn',     'ACT_PLS',  0, 'ST_TGS', 0x0001, 0x0001, 0],
+	[ 'Rumble',        'ACT_TGL',  0, 'ST_TGS', 0x0001, 0x0001, 1],
+	[ 'Laser',         'ACT_TGR',  4, 'ST_TGS', 0x0001, 0x0001, 2],
+	[ 'Shield',        'ACT_TGL',  0, 'ST_TGS', 0x0001, 0x0001, 3],
+	[ 'Shield Bounce', 'ACT_PLS',  0, 'ST_TGS', 0x0001, 0x0001, 4],
+	[ 'Hyperspace',    'ACT_PLS', 34, 'ST_TGS', 0x0001, 0x0001, 5],
+]
+
+SoundBoard = [
+	[ 'Armor Attack', len(AASounds), AASounds],
+	[ 'Rip Off',      len(ROSounds), ROSounds],
+	[ 'Solar Quest',  len(SQSounds), SQSounds],
+	[ 'Space Wars', len(SWSounds), SWSounds],
+	[ 'Star Castle',  len(SCSounds), SCSounds],
+	[ 'Star Hawk',    len(SHSounds), SHSounds],
+	[ 'Sundance',     len(SDSounds), SDSounds],
+	[ 'Tailgunner',   len(TGSounds), TGSounds]
+]
+
+GameBoards = {
+	'Armor Attack': {'sounds': AASounds, 'init': AAInit},
+	'Rip Off': {'sounds': ROSounds, 'init': AAInit},
+	'Solar Quest': {'sounds': SQSounds, 'init': AAInit},
+	'Space Wars': {'sounds': SWSounds, 'init': AAInit},
+	'Star Castle': {'sounds': SCSounds, 'init': AAInit},
+	'Star Hawk': {'sounds': SHSounds, 'init': AAInit},
+	'Sundance': {'sounds': SDSounds, 'init': AAInit},
+	'Tailgunner': {'sounds': TGSounds, 'init': AAInit}
+}
+
+class GameBoard():
+
+	def __init__(self):
+		self.AASounds = AASounds
+		self.ROSounds = ROSounds
+		self.SQSounds = SQSounds
+		self.SWSounds = SWSounds
+		self.SCSounds = SCSounds
+		self.SHSounds = SHSounds
+		self.SDSounds = SDSounds
+		self.TGSounds = TGSounds
+		
+		self.Boards = GameBoards
+		
+
